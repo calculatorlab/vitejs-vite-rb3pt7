1f2e95ea498f0ce9cba46cf648c55cca9af15095
Ashish Gupta <calculatorlab@yahoo.com>
2022-10-23 19:18:47 +0530

    Added gitignore entries

--- src/ui/Menu/MenuIconBar.tsx
+++ src/ui/Menu/MenuIconBar.tsx
@@ -1,36 +1,36 @@
-import React from 'react';
+import React from "react";
 
-import { range } from '../../util/seqgen';
+import { range } from "../../util/seqgen";
 
-import ContentCutIcon from '@mui/icons-material/ContentCut';
-import ContentCopyIcon from '@mui/icons-material/ContentCopy';
-import ContentPasteIcon from '@mui/icons-material/ContentPaste';
-import FormatBoldIcon from '@mui/icons-material/FormatBold';
-import FormatItalicIcon from '@mui/icons-material/FormatItalic';
-import FormatUnderlinedIcon from '@mui/icons-material/FormatUnderlined';
-import FormatAlignLeftIcon from '@mui/icons-material/FormatAlignLeft';
-import FormatAlignRightIcon from '@mui/icons-material/FormatAlignRight';
-import FormatAlignCenterIcon from '@mui/icons-material/FormatAlignCenter';
-import FormatColorFillIcon from '@mui/icons-material/FormatColorFill';
-import FormatColorTextIcon from '@mui/icons-material/FormatColorText';
+import ContentCutIcon from "@mui/icons-material/ContentCut";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
+import ContentPasteIcon from "@mui/icons-material/ContentPaste";
+import FormatBoldIcon from "@mui/icons-material/FormatBold";
+import FormatItalicIcon from "@mui/icons-material/FormatItalic";
+import FormatUnderlinedIcon from "@mui/icons-material/FormatUnderlined";
+import FormatAlignLeftIcon from "@mui/icons-material/FormatAlignLeft";
+import FormatAlignRightIcon from "@mui/icons-material/FormatAlignRight";
+import FormatAlignCenterIcon from "@mui/icons-material/FormatAlignCenter";
+import FormatColorFillIcon from "@mui/icons-material/FormatColorFill";
+import FormatColorTextIcon from "@mui/icons-material/FormatColorText";
 
-const MenuIconBar: React.FC = () => {
+const MenuIconBar: React.FC = (): JSX.Element => {
   return (
     <div className="h-9 flex-none flex items-center bg-gray-100">
       <IconWrapper>
-        <ContentCopyIcon key={'contentCopyIcon'} />
-        <ContentPasteIcon key={'contentPasteIcon'} />
-        <ContentCutIcon key={'contentCutIcon'} />
-        <FontFamilySelector key={'fontFamilySelector'} />
-        <FontSizeSelector key={'fontSizeSelector'} />
-        <FormatBoldIcon key={'formatBoldIcon'} />
-        <FormatItalicIcon key={'formatItalicIcon'} />
-        <FormatUnderlinedIcon key={'formatUnderlinedIcon'} />
-        <FormatAlignLeftIcon key={'formatAlignLeftIcon'} />
-        <FormatAlignCenterIcon key={'formatAlignCenterIcon'} />
-        <FormatAlignRightIcon key={'formatAlignRightIcon'} />
-        <FormatColorFillIcon key={'formatColorFillIcon'} />
-        <FormatColorTextIcon key={'formatColorTextIcon'} />
+        <ContentCopyIcon key={"contentCopyIcon"} />
+        <ContentPasteIcon key={"contentPasteIcon"} />
+        <ContentCutIcon key={"contentCutIcon"} />
+        <FontFamilySelector key={"fontFamilySelector"} />
+        <FontSizeSelector key={"fontSizeSelector"} />
+        <FormatBoldIcon key={"formatBoldIcon"} />
+        <FormatItalicIcon key={"formatItalicIcon"} />
+        <FormatUnderlinedIcon key={"formatUnderlinedIcon"} />
+        <FormatAlignLeftIcon key={"formatAlignLeftIcon"} />
+        <FormatAlignCenterIcon key={"formatAlignCenterIcon"} />
+        <FormatAlignRightIcon key={"formatAlignRightIcon"} />
+        <FormatColorFillIcon key={"formatColorFillIcon"} />
+        <FormatColorTextIcon key={"formatColorTextIcon"} />
       </IconWrapper>
     </div>
   );
@@ -52,21 +52,21 @@ const IconWrapper: React.FC<{ children: React.ReactNode }> = (props) => (
 );
 
 const fontSelectorValues: string[] = [
-  'Noto Sans',
-  'Arial',
-  'Calibri',
-  'Comic Sans MS',
-  'Courier New',
-  'Impact',
-  'Georgia',
-  'Garamond',
-  'Lato',
-  'Open Sans',
-  'Palatino',
-  'Verdana',
+  "Noto Sans",
+  "Arial",
+  "Calibri",
+  "Comic Sans MS",
+  "Courier New",
+  "Impact",
+  "Georgia",
+  "Garamond",
+  "Lato",
+  "Open Sans",
+  "Palatino",
+  "Verdana",
 ];
 
-const FontFamilySelector: React.FC = () => {
+const FontFamilySelector: React.FC = (): JSX.Element => {
   return (
     <select title="font-selector" className="h-4/5 w-40 text-lg">
       {fontSelectorValues.map((v) => (
@@ -78,8 +78,8 @@ const FontFamilySelector: React.FC = () => {
   );
 };
 
-const FontSizeSelector: React.FC = () => {
-  const fontRange = range(10, 30, 2);
+const FontSizeSelector: React.FC = (): JSX.Element => {
+  const fontRange: string[] = range(10, 30, 2);
   return (
     <select title="font-size-selector" className="h-4/5 text-lg">
       {fontRange.map((v) => (
